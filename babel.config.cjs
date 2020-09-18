fd755d934cae4faaab1773db1400a83eb7a31bf4
Christian Sterzl <christian.sterzl@gmail.com>
2020-09-18 17:41:15 +0200

    update deps
    Signed-off-by: Christian Sterzl <christian.sterzl@gmail.com>

--- babel.config.cjs
+++ babel.config.cjs
@@ -6,6 +6,5 @@ module.exports = {
   plugins: [
     ['@babel/plugin-proposal-decorators', {decoratorsBeforeExport: true}],
     ['@babel/plugin-proposal-class-properties', {loose: true}],
-    'babel-plugin-parameter-decorator',
   ],
 };
