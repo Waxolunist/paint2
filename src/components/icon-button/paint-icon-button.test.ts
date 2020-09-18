f24eff5f7e673b25aee6b72c83026ff5c4c3726c
Christian Sterzl <christian.sterzl@gmail.com>
2020-09-18 17:34:18 +0200

    remove karma etc
    Signed-off-by: Christian Sterzl <christian.sterzl@gmail.com>

--- src/components/icon-button/paint-icon-button.test.ts
+++ src/components/icon-button/paint-icon-button.test.ts
@@ -2,26 +2,24 @@ import {IconButton} from './paint-icon-button';
 import {fixture, html, oneEvent} from '@open-wc/testing';
 import {firePointerEvent} from '../../test/pointerevents';
 
-const expect = chai.expect;
 describe('paint-icon-button', () => {
   it('is defined', () => {
     const el = document.createElement('paint-icon-button');
-    expect(el).to.be.an.instanceof(IconButton);
+    expect(el).toBeInstanceOf(IconButton);
   });
 
   it('renders correctly', async () => {
     const element = await fixture(
       html`<paint-icon-button></paint-icon-button>`
     );
-    expect(element).shadowDom.to.equalSnapshot();
+    expect(element.shadowRoot!.innerHTML).toMatchSnapshot();
   });
 
   it('renders correctly when active and with slot', async () => {
     const element = await fixture(
       html`<paint-icon-button active>Foo</paint-icon-button>`
     );
-    expect(element).shadowDom.to.equalSnapshot();
-    expect(element).lightDom.to.equalSnapshot();
+    expect(element.shadowRoot!.innerHTML).toMatchSnapshot();
   });
 
   it('renders correctly after pointerdown', async () => {
@@ -29,7 +27,7 @@ describe('paint-icon-button', () => {
       html`<paint-icon-button></paint-icon-button>`
     );
     await firePointerEvent(element, ['down']);
-    expect(element).shadowDom.to.equalSnapshot();
+    expect(element.shadowRoot!.innerHTML).toMatchSnapshot();
   });
 
   it('renders correctly after pointerdown and up', async () => {
@@ -37,13 +35,13 @@ describe('paint-icon-button', () => {
       html`<paint-icon-button></paint-icon-button>`
     );
     await firePointerEvent(element, ['down', 'up']);
-    expect(element).shadowDom.to.equalSnapshot();
+    expect(element.shadowRoot!.innerHTML).toMatchSnapshot();
   });
 
   it('fires event after pointer up', async () => {
     const el = await fixture(html`<paint-icon-button></paint-icon-button>`);
     firePointerEvent(el, ['down', 'up']);
     const {detail} = await oneEvent(el, 'icon-clicked');
-    expect(detail).to.be.not.undefined;
+    expect(detail).not.toBeUndefined();
   });
 });
