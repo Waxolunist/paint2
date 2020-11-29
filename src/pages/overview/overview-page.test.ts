7c5d01c4fad7f0b5ca95339f48828cb404ff5725
Christian Sterzl <christian.sterzl@gmail.com>
2020-11-29 07:48:51 +0100

    Overview tests
    Signed-off-by: Christian Sterzl <christian.sterzl@gmail.com>

--- src/pages/overview/overview-page.test.ts
+++ src/pages/overview/overview-page.test.ts
@@ -1,6 +1,7 @@
 import {html} from 'lit-html';
 import {OverviewPage} from './overview-page';
-import {fixture} from '@open-wc/testing';
+import {elementUpdated, fixture} from '@open-wc/testing';
+import {PaintingImpl} from '../../ducks/paint-model';
 
 describe('overview-page', () => {
   it('is defined', () => {
@@ -13,5 +14,17 @@ describe('overview-page', () => {
       html`<paint-overview-page></paint-overview-page>`
     );
     expect(element.shadowRoot!.innerHTML).toMatchSnapshot();
+  });
+
+  it('renders correctly with paintings', async () => {
+    const element: OverviewPage = await fixture(
+      html`<paint-overview-page></paint-overview-page>`
+    );
+    element.paintings = [new PaintingImpl(), new PaintingImpl()];
+    await elementUpdated(element);
+    expect(element.shadowRoot?.innerHTML).toMatchSnapshot();
+    expect(
+      element.shadowRoot?.querySelectorAll('paint-paint-button')
+    ).toHaveLength(2);
   });
 });
