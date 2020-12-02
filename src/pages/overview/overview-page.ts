aca04eddb900d52b8ee4ec72c5eccb5a5bfd837f
Christian Sterzl <christian.sterzl@gmail.com>
2020-12-02 08:52:50 +0100

    Add animations for remove
    Signed-off-by: Christian Sterzl <christian.sterzl@gmail.com>

--- src/pages/overview/overview-page.ts
+++ src/pages/overview/overview-page.ts
@@ -6,6 +6,7 @@ import {
   property,
   CSSResult,
   TemplateResult,
+  queryAll,
 } from 'lit-element';
 import '../../components/new-paint-button/new-paint-button';
 import '../../components/paint-button/paint-button';
@@ -15,6 +16,7 @@ import store from '../../store';
 import {connect} from 'pwa-helpers/connect-mixin';
 import {Painting, PaintState, defaultDataUrl} from '../../ducks/paint-model';
 import {repeat} from 'lit-html/directives/repeat';
+import {ifDefined} from 'lit-html/directives/if-defined';
 import {
   loadData,
   newPainting,
@@ -23,9 +25,29 @@ import {
 } from '../../ducks/paint';
 import {toFileExtension, dataURLtoBlob} from '../../ducks/paint-utils';
 import {RouterState} from 'lit-redux-router/lib/reducer';
+import {AnimatedStyles} from '../../styles/shared-styles';
+import {directive, PropertyPart} from 'lit-html';
+
+interface PaintingAnimationData {
+  id: number;
+  index: number;
+  clientRect: DOMRect;
+}
+
+const elementDirtyCheck = directive((value) => (part: PropertyPart): void => {
+  const {name, element} = part.committer;
+  if (value !== element[name as keyof Element]) {
+    part.setValue(value);
+  }
+});
 
 @customElement('paint-overview-page')
 export class OverviewPage extends connect(store)(LitElement) {
+  @queryAll('paint-paint-button')
+  paintingButtons?: NodeList;
+
+  private currentPaintingClientRects: PaintingAnimationData[] = [];
+
   @property({type: Array, attribute: false})
   paintings: Painting[] = [];
 
@@ -39,74 +61,92 @@ export class OverviewPage extends connect(store)(LitElement) {
     if (router.routes['/']?.active) this.paintings = paint.paintings;
   }
 
-  static get styles(): CSSResult {
-    // language=CSS
-    return css`
-      :host {
-        display: block;
-        background-color: #91b5ff;
-        padding: 1.5em 0;
-        /* prettier-ignore */
-        min-width: calc((var(--painting-width) / var(--painting-scalefactor) + var(--painting-margin)) * 2);
-        position: relative;
-      }
+  updated(changedProperties: Map<string, Painting[]>): void {
+    if (this.paintingDeleted(changedProperties.get('paintings'))) {
+      this.updateComplete.then(this.animatePaintings);
+    }
+  }
 
-      .paintings {
-        display: grid;
-        grid-gap: var(--painting-margin);
-        grid-template-columns: repeat(
-          auto-fit,
-          calc(var(--painting-width) / var(--painting-scalefactor))
-        );
-        justify-content: center;
-      }
-
-      .painting {
-        width: calc(var(--painting-width) / var(--painting-scalefactor));
-        height: calc(var(--painting-height) / var(--painting-scalefactor));
-        position: relative;
-      }
-
-      .about {
-        position: absolute;
-        bottom: -2em;
-        padding: 1em;
-        font-size: large;
-        font-family: monospace;
-        font-weight: bold;
-      }
-
-      .about a {
-        color: #ffffef;
-      }
-
-      @media screen and (max-width: 400px) {
+  static get styles(): CSSResult[] {
+    return [
+      AnimatedStyles,
+      css`
         :host {
+          display: block;
+          background-color: #91b5ff;
           padding: 1.5em 0;
-          --painting-margin: 1em;
+          /* prettier-ignore */
+          min-width: calc((var(--painting-width) / var(--painting-scalefactor) + var(--painting-margin)) * 2);
+          position: relative;
         }
 
         .paintings {
-          --painting-scalefactor: 1.3;
-          grid-column-gap: 1.3em;
-          grid-row-gap: 1.3em;
+          display: grid;
+          grid-gap: var(--painting-margin);
+          grid-template-columns: repeat(
+            auto-fit,
+            calc(var(--painting-width) / var(--painting-scalefactor))
+          );
+          justify-content: center;
         }
-      }
-    `;
+
+        .painting {
+          width: calc(var(--painting-width) / var(--painting-scalefactor));
+          height: calc(var(--painting-height) / var(--painting-scalefactor));
+          position: relative;
+          transition-property: transform;
+          will-change: transform;
+        }
+
+        .painting.translate {
+          transform: translate(0px, 0px);
+        }
+
+        .about {
+          position: absolute;
+          bottom: -2em;
+          padding: 1em;
+          font-size: large;
+          font-family: monospace;
+          font-weight: bold;
+        }
+
+        .about a {
+          color: #ffffef;
+        }
+
+        @media screen and (max-width: 400px) {
+          :host {
+            padding: 1.5em 0;
+            --painting-margin: 1em;
+          }
+
+          .paintings {
+            --painting-scalefactor: 1.3;
+            grid-column-gap: 1.3em;
+            grid-row-gap: 1.3em;
+          }
+        }
+      `,
+    ];
   }
 
   render(): TemplateResult {
     return html`
       <div class="paintings">
         <paint-new-paint-button
-          class="painting"
+          class="painting translate"
           @paint-clicked="${this.newPainting}"
         ></paint-new-paint-button>
         ${repeat(
           this.paintings,
-          (painting) =>
+          (painting, index) =>
             html`<paint-paint-button
-              class="painting"
+              class="painting translate"
+              data-id="${ifDefined(painting.id)}"
+              style="${elementDirtyCheck(
+                this.calculateTranslateProperty(index, painting.id)
+              )}"
               @paint-clicked="${this.openPainting(painting.id)}"
               imageUrl="${painting.blobUrl}"
             >
@@ -133,25 +173,26 @@ export class OverviewPage extends connect(store)(LitElement) {
     `;
   }
 
-  private newPainting(): void {
+  private newPainting = (): void => {
     (<ThunkDispatch>store.dispatch)(newPainting());
-  }
+  };
 
-  private openPainting = (id?: number | string) => (e: CustomEvent): void => {
-    const domRect = (<HTMLElement>e.target).getBoundingClientRect();
-    this.dispatchEvent(
-      new CustomEvent('button-clicked', {
-        detail: {
-          domRect,
-        },
-        bubbles: true,
-        composed: true,
-      })
-    );
+  private openPainting = (id?: number | string) => (): void => {
     (<ThunkDispatch>store.dispatch)(loadData(id));
   };
 
-  private removePainting = (id?: number | string) => (): void => {
+  private removePainting = (
+    id?: number | string
+  ) => async (): Promise<void> => {
+    this.currentPaintingClientRects = [];
+    this.paintingButtons?.forEach((node, index) => {
+      this.currentPaintingClientRects.push({
+        id: this.paintings[index].id ?? 0,
+        index: index,
+        clientRect: (node as HTMLElement).getBoundingClientRect(),
+      });
+    });
+    this.removeAnimatedClass();
     (<ThunkDispatch>store.dispatch)(removePainting(id));
   };
 
@@ -185,11 +226,54 @@ export class OverviewPage extends connect(store)(LitElement) {
           ],
           title: 'My Painting',
           text: `Look Ma' what I did.`,
-          url: 'myurl',
+          url: '/',
         });
       } catch (error) {
         console.error('Sharing failed', error);
       }
     }
+  };
+
+  private paintingDeleted = (oldVal: Painting[] = []): boolean =>
+    !!oldVal && oldVal.length > this.paintings.length;
+
+  private calculateTranslateProperty = (index: number, id?: number): string => {
+    const dataWithIndexAndId = this.currentPaintingClientRects.find(
+      (p) => p.index === index && p.id === id
+    );
+    if (dataWithIndexAndId || this.currentPaintingClientRects.length === 0) {
+      return 'transform: translate(0px, 0px)';
+    }
+    const dataWithIdIndex = this.currentPaintingClientRects.findIndex(
+      (p) => p.id === id
+    );
+    const current = this.currentPaintingClientRects[dataWithIdIndex];
+    const predeccessor = this.currentPaintingClientRects[dataWithIdIndex - 1];
+    if (current && predeccessor) {
+      const translateX = current.clientRect.x - predeccessor.clientRect.x;
+      const translateY = current.clientRect.y - predeccessor.clientRect.y;
+      return `transform: translate(${translateX}px, ${translateY}px)`;
+    }
+    return 'transform: translate(0px, 0px)';
+  };
+
+  private animatePaintings = (): void => {
+    this.paintingButtons?.forEach((node) => {
+      const htmlel = node as HTMLElement;
+      setTimeout(
+        () =>
+          requestAnimationFrame(() => {
+            htmlel.style.transform = 'translate(0px, 0px)';
+            htmlel.classList.add('animated');
+          }),
+        200
+      );
+    });
+  };
+
+  private removeAnimatedClass = (): void => {
+    this.paintingButtons?.forEach((node) =>
+      (node as HTMLElement).classList.remove('animated')
+    );
   };
 }
