4a49b0421b0c2c249f6cf3def5701b01e6748351
Christian Sterzl <christian.sterzl@gmail.com>
2020-12-05 10:00:36 +0100

    Debug information
    Signed-off-by: Christian Sterzl <christian.sterzl@gmail.com>

--- scripts/build_app.sh
+++ scripts/build_app.sh
@@ -7,4 +7,13 @@ ls -la
 echo "<====================>"
 
 yes | ../.tools/tools/bin/sdkmanager --sdk_root=.tools --licenses
+
+echo "Debug Information:"
+echo "Current Dir: $(pwd)"
+echo "<====================>"
+echo "Tools content"
+echo "<====================>"
+ls -la ../.tools
+echo "<====================>"
+
 `npm bin`/bubblewrap build --skipPwaValidation
