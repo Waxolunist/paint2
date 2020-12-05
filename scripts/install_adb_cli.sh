77d0f87e97797eeaa8865277e82149ce1d6c9eb1
Christian Sterzl <christian.sterzl@gmail.com>
2020-12-05 09:11:07 +0100

    Added debug information - cwd not working seems
    Signed-off-by: Christian Sterzl <christian.sterzl@gmail.com>

--- scripts/install_adb_cli.sh
+++ scripts/install_adb_cli.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+echo "Debug Information:"
+echo "Current Dir: $(pwd)"
+echo "<====================>"
+ls -la
+echo "<====================>"
+
 DEFAULT_SYSTEM="linux"
 # Possible values: mac | linux | win
 SYSTEM=${1:-$DEFAULT_SYSTEM}    
@@ -7,7 +13,20 @@ echo "Download commandline tools for ${SYSTEM}."
 # Download from https://developer.android.com/studio#command-tools
 mkdir -p .tools
 cd .tools
+
+echo "Debug Information:"
+echo "Current Dir: $(pwd)"
+echo "<====================>"
+ls -la
+echo "<====================>"
+
 curl https://dl.google.com/android/repository/commandlinetools-${SYSTEM}-6858069_latest.zip --output commandlinetools.zip
 unzip -u commandlinetools.zip
 rm -rf tools
 cp -R cmdline-tools tools
+
+echo "Debug Information:"
+echo "Current Dir: $(pwd)"
+echo "<====================>"
+ls -la
+echo "<====================>"
