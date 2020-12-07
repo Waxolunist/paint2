52afd45d9883006dff498a4c9a1dd7925509623f
Christian Sterzl <christian.sterzl@gmail.com>
2020-12-07 16:06:44 +0100

    Test build with android home
    Signed-off-by: Christian Sterzl <christian.sterzl@gmail.com>

--- scripts/config_bubblewrap.sh
+++ scripts/config_bubblewrap.sh
@@ -5,9 +5,10 @@ then
     JDK8_HOME=$(cd "`/usr/libexec/java_home -v1.8`/../.." && pwd)
 fi
 CWD=$(pwd)
+
 ANDROID_CLI_HOME="${CWD}/.tools"
 
 mkdir -p "${HOME}/.bubblewrap"
-echo "{\"jdkPath\":\"${JDK8_HOME:-$JAVA_HOME}\",\"androidSdkPath\":\"${ANDROID_CLI_HOME}\"}" > "${HOME}/.bubblewrap/config.json"
+echo "{\"jdkPath\":\"${JDK8_HOME:-$JAVA_HOME}\",\"androidSdkPath\":\"${ANDROID_HOME:-$ANDROID_CLI_HOME}\"}" > "${HOME}/.bubblewrap/config.json"
 
 cat "${HOME}/.bubblewrap/config.json"
