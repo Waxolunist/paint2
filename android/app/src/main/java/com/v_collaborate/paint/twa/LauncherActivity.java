5e3d2bed2d60fe5ab577576322fc785ea066d02f
Christian Sterzl <christian.sterzl@iteratec.com>
2021-08-25 09:12:10 +0200

    Update Android TWA
    Signed-off-by: Christian Sterzl <christian.sterzl@iteratec.com>

--- android/app/src/main/java/com/v_collaborate/paint/twa/LauncherActivity.java
+++ android/app/src/main/java/com/v_collaborate/paint/twa/LauncherActivity.java
@@ -15,7 +15,10 @@
  */
 package com.v_collaborate.paint.twa;
 
+import android.content.pm.ActivityInfo;
 import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
 
 
 
@@ -24,6 +27,20 @@ public class LauncherActivity
     
 
     
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Setting an orientation crashes the app due to the transparent background on Android 8.0
+        // Oreo and below. We only set the orientation on Oreo and above. This only affects the
+        // splash screen and Chrome will still respect the orientation.
+        // See https://github.com/GoogleChromeLabs/bubblewrap/issues/496 for details.
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.O) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        } else {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+        }
+    }
 
     @Override
     protected Uri getLaunchingUrl() {
